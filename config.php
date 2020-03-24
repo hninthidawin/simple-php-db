5184e741fb19b3650d6e93e307f7b69e94ef11c1
Win <hninthida.win@fujitsu.com>
2020-03-24 07:10:00 +0100

    simple php application read/write to db

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("databaseusername");
+$dbpwd = getenv("databasepassword");
+$dbname = getenv("databasename");
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
